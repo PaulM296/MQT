e330fff48ca6f699cc7a7a3fe533f407907f932d
PaulM <paulmicluta@gmail.com>
2023-10-31 16:19:15 +0200

    Individual folder for lab 5 added,

---
+++ Lab5/Lab3_Java_project/src/main/java/producer/Company.java
@@ -0,0 +1,34 @@
+package producer;
+
+public class Company {
+    private String company;
+    int tradeNumber;
+    String registeredName;
+
+    public Company() {
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getTradeNumber() {
+        return tradeNumber;
+    }
+
+    public void setTradeNumber(int tradeNumber) {
+        this.tradeNumber = tradeNumber;
+    }
+
+    public String getRegisteredName() {
+        return registeredName;
+    }
+
+    public void setRegisteredName(String registeredName) {
+        this.registeredName = registeredName;
+    }
+}
